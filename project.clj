dab7050c27d459a31bb73962e8b0e9079f2f8c5b
Nicolas Kosinski <nicokosi@yahoo.com>
2018-01-25 07:02:39 +0100

    Upgrade all libraries to latest: clj-http to 3.7.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojure "1.9.0"],
                  [incanter "1.5.7"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "2.3.0"]
+                 [clj-http "3.7.0"]
                  [slingshot "0.12.2"]
                  ]
   :main ^:skip-aot strava-activity-graphs.core
