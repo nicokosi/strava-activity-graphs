daa0fb861cc864df28c46fe687cfed5eb41a0808
Nicolas Kosinski <nicokosi@yahoo.com>
2020-07-02 21:17:34 +0200

    Bump incanter to 1.9.3
    Use column indices instead of column names because datasets have no column names anymore:
    https://github.com/incanter/incanter/wiki/Incanter-2.0-change-log#dataset

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Creative Commons Attribution 4.0"
             :url  "https://creativecommons.org/licenses/by/4.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [incanter/incanter-core "1.5.7"]
-                 [incanter/incanter-charts "1.5.7"]
-                 [incanter/incanter-io "1.5.7"]
+                 [incanter/incanter-core "1.9.3"]
+                 [incanter/incanter-charts "1.9.3"]
+                 [incanter/incanter-io "1.9.3"]
                  [org.clojure/data.json "0.2.7"]
                  [clj-http "3.10.2"]
                  [slingshot "0.12.2"]]
